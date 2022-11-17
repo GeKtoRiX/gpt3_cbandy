fe9b787da79d16498869962a237e152c7ed17ddb
ARTEM CB@NDY <cb@ndy@gmail.com>
2022-11-17 22:38:58 +0300

    navbar lg+

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p>
+            <a href='#home'></a>Home
+          </p>
+          <p>
+            <a href='#wgpt3'></a>What is GPT?
+          </p>
+          <p>
+            <a href='#possibility'></a>Open AI
+          </p>
+          <p>
+            <a href='#features'></a>Case Studies
+          </p>
+          <p>
+            <a href='#blog'></a>Library
+          </p>
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
